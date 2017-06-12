7dc32ca672cfe1cfbdf8408d61c8cbd3de53723a
sidneyseay <sidneyeseay@gmail.com>
2017-06-12 06:42:25 -0400

    Class 2 Updates

---
+++ app/src/main/java/com/example/sidneyseay/sidney2017summer/fragment/DemoFragment.java
@@ -0,0 +1,91 @@
+package com.example.sidneyseay.sidney2017summer.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.sidneyseay.sidney2017summer.R;
+import com.example.sidneyseay.sidney2017summer.adapter.ListNormalAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DemoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DemoFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private final ArrayList<String> contentList;
+    private ListView listView;
+
+
+    public DemoFragment() {
+        contentList = new ArrayList<String>();
+        contentList.add("ViewPager");
+        contentList.add("ImageScaleType");
+        contentList.add("9Patch");
+        contentList.add("A");
+        contentList.add("B");
+        contentList.add("C");
+        contentList.add("D");
+        contentList.add("E");
+        contentList.add("F");
+        contentList.add("G");
+        contentList.add("H");
+        contentList.add("I");
+        contentList.add("J");
+        contentList.add("K");
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DemoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DemoFragment newInstance(String param1, String param2) {
+        DemoFragment fragment = new DemoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_demo, container, false);
+        listView = (ListView)view.findViewById(R.id.fragment_demo_lv);
+        ListNormalAdapter adapter = new ListNormalAdapter(this.getContext(), contentList);
+        listView.setAdapter(adapter);
+
+        return view;
+    }
+
+}
